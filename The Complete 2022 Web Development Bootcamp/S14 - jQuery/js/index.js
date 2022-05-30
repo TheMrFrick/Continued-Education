59053f5f17b6a1afabade23aaf7eeb6bcdd01022
Kyle Frick <mrfrick1@hotmail.com>
2022-05-30 08:23:57 -0400

    feat: S14 jQuery done

---
+++ The Complete 2022 Web Development Bootcamp/S14 - jQuery/js/index.js
@@ -0,0 +1,32 @@
+// Document ready allows the javascript to be put into the header as it will wait for the site to be fully loaded before acting.
+$(document).ready(() => {
+    // Add Classes
+    // $('h1').addClass('big-title margin-50');
+
+    // Check for classes
+    // $('h1').hasClass('big-title');
+
+    // Manipulate Text
+    // $('h1').text('bye');
+
+    // Manipulate HTML
+    // $('button').html('<em>Hey</em>')
+
+    // Manipulating Attributes
+    // $('button').attr('type', 'submit');
+
+    // Add event listener
+    // $('h1').click(() => {
+    //     $('h1').css('color', 'purple');
+    // })
+
+    // Adding Elements
+    // $('h1').before('<button>Before</button>');
+    // $('h1').after('<button>After</button>');
+
+    // Animating
+    // $('h1').slideUp().slideDown().animate({opacity: 0.5});
+
+    // Selects all the buttons
+    // $("button");
+})
