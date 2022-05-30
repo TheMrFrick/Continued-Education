aa5e38a04ca0fe461e9d03d770eb610bec2e6e9e
Kyle Frick <mrfrick1@hotmail.com>
2022-05-30 16:38:38 -0400

    feat: S18 completed

---
+++ The Complete 2022 Web Development Bootcamp/S18 - What is Node.js/index.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+console.log(superheroes.random(), 'vs', supervillains.random());
